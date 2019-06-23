77a94ef2c0878041f305b575f00cd9c21978d031
Alexander Zinchuk <alx.zinchuk@gmail.com>
2019-06-22 20:38:39 -0700

    Updates

---
+++ precache-manifest.76828b651a841dca97e973dcb727c939.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec03084a4fa4a837a76",
+    "url": "/telegram-react/static/css/6.6529eae8.chunk.css"
+  },
+  {
+    "revision": "7fe85e731c72c317c43b",
+    "url": "/telegram-react/static/css/main.f80ac61d.chunk.css"
+  },
+  {
+    "revision": "e63200a449ccbfbc279b",
+    "url": "/telegram-react/static/js/runtime~main.6ce96a56.js"
+  },
+  {
+    "revision": "4d0799650822a5c60f22",
+    "url": "/telegram-react/static/js/2.f0c349ee.chunk.js"
+  },
+  {
+    "revision": "f01cec9bccc80a678ed0ec04acecade2",
+    "url": "/telegram-react/static/media/IconsetW_2x.f01cec9b.png"
+  },
+  {
+    "revision": "56f184756d39d654ffc0",
+    "url": "/telegram-react/static/js/3.6a5ad006.chunk.js"
+  },
+  {
+    "revision": "70d7799ff6a8a3d21c79",
+    "url": "/telegram-react/static/js/4.f10702f2.chunk.js"
+  },
+  {
+    "revision": "5270d71cd78fd282a3736b0e6ae7f048",
+    "url": "/telegram-react/static/media/General_2x.5270d71c.png"
+  },
+  {
+    "revision": "f958811125ee703c622b",
+    "url": "/telegram-react/static/js/5.08e1bd87.chunk.js"
+  },
+  {
+    "revision": "7fe85e731c72c317c43b",
+    "url": "/telegram-react/static/js/main.aefa693a.chunk.js"
+  },
+  {
+    "revision": "aec03084a4fa4a837a76",
+    "url": "/telegram-react/static/js/6.b21bb486.chunk.js"
+  },
+  {
+    "revision": "c51398624ae18e93f733",
+    "url": "/telegram-react/static/js/7.c847218f.chunk.js"
+  },
+  {
+    "revision": "8f34cd20fc1bbebf2dcf",
+    "url": "/telegram-react/static/js/8.c4a41c8a.chunk.js"
+  },
+  {
+    "revision": "7a71629a5e4f7482b6320b103f1d543c",
+    "url": "/telegram-react/static/media/Manytabs_2x.7a71629a.png"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "f958811125ee703c622b",
+    "url": "/telegram-react/static/css/5.d2963440.chunk.css"
+  },
+  {
+    "revision": "56f184756d39d654ffc0",
+    "url": "/telegram-react/static/css/3.4abf299b.chunk.css"
+  },
+  {
+    "revision": "6826e76105d87d43499bfb8f2f3cfa87",
+    "url": "/telegram-react/index.html"
+  }
+];
